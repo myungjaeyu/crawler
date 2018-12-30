bb7c9489f47de3780f68c1341a1f29c62c714b71
myungjaeyu <rootcode10@gmail.com>
2018-12-30 12:45:58 +0900

    Added cart script.

--- bin/index.js
+++ bin/index.js
@@ -53,6 +53,33 @@ async function mytmon() {
 }
 
 
+async function cart() {
+
+    const url = 'http://order.ticketmonster.co.kr/m/cart'
+
+    const { data } = await axios.get(url, { jar, withCredentials: true })
+
+    const parser = (element, content) => Object.keys(content).reduce((acc, cur) => {
+        acc[cur] = element.querySelector(content[cur]).textContent
+        return acc
+    }, {})
+
+
+    const dom = new JSDOM(data)
+
+    const list = Array.from(dom.window.document.querySelectorAll('.prod')).map(elem => ({
+        ...parser(elem, {
+            name : '.prod_name', 
+            price : '.price .num',
+            quantity : '.quantity',
+        }),
+        url : 'http://mobile.ticketmonster.co.kr/deals/' + elem.dataset.dealSrl
+    }))
+
+    console.log(list)
+}
+
+
 /* bootstrap */
 (async () => {
 
@@ -60,5 +87,6 @@ async function mytmon() {
 
     await login(ID_TOKEN, PW_TOKEN)
     await mytmon()
+    await cart()
 
 })()
